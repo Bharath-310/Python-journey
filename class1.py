35e09f8ea62552a5e7911f88bf17b0c81517092a
Bharath-310 <100527419+Bharath-310@users.noreply.github.com>
2024-08-30 20:56:22 +0530

    class1.py

--- class1.py
+++ class1.py
@@ -3,12 +3,12 @@ class Wordset:
    def __init__(self):
          self.words=set() 
          
-    def addtext(self,text): 
+   def addtext(self,text): 
         text=self.cleantext(text) 
         for word in text.split(): 
             self.words.add(word)
         
-    def cleantext(self,text): 
+   def cleantext(self,text): 
         text=text.replace('/','').replace(',','').replace('.','').replace('!','') 
         return text.lower()  
         
